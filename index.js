5f578c11eee7bde7c59cc79d6aa6a8ac52c2676c
elertan <denkievits@gmail.com>
2018-02-02 23:17:00 +0100

    setup

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/*', function (req, res) {
+    res.render('index.html');
+});
+
+app.listen(1337);
